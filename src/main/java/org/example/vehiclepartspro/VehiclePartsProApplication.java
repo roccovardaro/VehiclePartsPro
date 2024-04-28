5f8c5d41c151104c7130e0ef0292f74f658fc005
Rocco Pio <roccovardaro17@gmail.com>
2024-04-28 17:08:23 +0200

    Progetto VehiclePartPro

---
+++ src/main/java/org/example/vehiclepartspro/VehiclePartsProApplication.java
@@ -0,0 +1,13 @@
+package org.example.vehiclepartspro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiclePartsProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehiclePartsProApplication.class, args);
+    }
+
+}
